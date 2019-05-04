879a3d5f6b3b02ca8039275f2deb257ac77b12e1
Treydog14 <Treydog2014@gmail.com>
2019-05-03 20:40:43 -0700

    Finished.

--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -6,11 +6,26 @@
 
 def letterGrade(score):
     # your code here
+    if score <= 1:
+        grade = "F"
+    elif score == 2:
+        grade = "D"
+    elif score == 3:
+        grade = "C"
+    elif score == 4:
+        grade = "B"
+    elif score == 5:
+        grade = "A"
+    else:
+        grade = "Invalid value"
+
 
     return grade
 
 def unitTest():
     # your test code here
+    for score in range(7):
+        print("Score:", score, "returns a grade of:", letterGrade(score))
 
 if __name__ == "__main__":
     unitTest()
